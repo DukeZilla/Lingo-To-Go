6ec98656344d0accc2543916800d60c1582380c9
DukeZilla <59232365+DukeZilla@users.noreply.github.com>
2022-04-26 21:23:34 -0500

    Add files via upload

---
+++ updater.ps1
@@ -0,0 +1,12 @@
+cd ..
+$pwd = (pwd).path
+$source = "https://github.com/DukeZilla/Lingo-To-Go/archive/refs/heads/main.zip"
+$destination = "$env:USERPROFILE\Downloads\Lingo-To-Go.zip"
+Invoke-WebRequest $source -OutFile $destination
+cd \
+cd $env:USERPROFILE\Downloads
+Expand-Archive -LiteralPath $env:USERPROFILE\Downloads\Lingo-To-Go.zip -DestinationPath "$pwd\Lingo To Go"
+echo Done.
+echo " "
+start-sleep 3
+exit
