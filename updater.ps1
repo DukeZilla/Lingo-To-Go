85ad1e8e7c4343353821f3a8518da030234c717f
DukeZilla <59232365+DukeZilla@users.noreply.github.com>
2022-04-27 19:20:21 -0500

    Add files via upload

--- updater.ps1
+++ updater.ps1
@@ -1,11 +1,11 @@
 cd ..
-$pwd = (pwd).path
+$p = (pwd).path
 $source = "https://github.com/DukeZilla/Lingo-To-Go/archive/refs/heads/main.zip"
 $destination = "$env:USERPROFILE\Downloads\Lingo-To-Go.zip"
 Invoke-WebRequest $source -OutFile $destination
 cd \
 cd $env:USERPROFILE\Downloads
-Expand-Archive -LiteralPath $env:USERPROFILE\Downloads\Lingo-To-Go.zip -DestinationPath "$pwd\Lingo To Go"
+Expand-Archive -LiteralPath $env:USERPROFILE\Downloads\Lingo-To-Go.zip -DestinationPath "$p\Lingo To Go"
 echo Done.
 echo " "
 start-sleep 3
