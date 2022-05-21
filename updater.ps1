f290d5a81fe374f32b026405bafc8f7c3f70e2cf
DukeZilla <59232365+DukeZilla@users.noreply.github.com>
2022-05-21 12:17:58 -0500

    Add files via upload

--- updater.ps1
+++ updater.ps1
@@ -5,7 +5,7 @@ $destination = "$env:USERPROFILE\Downloads\Lingo-To-Go.zip"
 Invoke-WebRequest $source -OutFile $destination
 cd \
 cd $env:USERPROFILE\Downloads
-Expand-Archive -LiteralPath $env:USERPROFILE\Downloads\Lingo-To-Go.zip -DestinationPath "$p\Lingo To Go"
+Expand-Archive -LiteralPath "$env:USERPROFILE\Downloads\Lingo-To-Go.zip" -DestinationPath "$p\Lingo To Go"
 echo Done.
 echo " "
 start-sleep 3
